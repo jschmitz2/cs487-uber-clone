f7c2db8b89c4ca3d732aa641b98cb51292105069
Justin Schmitz <jschmitz2@hawk.iit.edu>
2021-04-08 18:04:47 -0500

    Initial frontend work linking in map

--- cs487-uber-ola/src/views/home.js
+++ cs487-uber-ola/src/views/home.js
@@ -2,61 +2,139 @@ import React from "react";
 import styled from "styled-components";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import keys from "../keys";
 
 const HeadingStyled = styled.h1``;
 
 const ContentDiv = styled.div`
   width: 100%;
   display: flex;
-
+  padding: 20px;
 `;
 
-const LeftPane = styled.div`
+const Pane = styled.div`
   max-width: 400px;
+  min-width: 300px;
+  padding-left: 20px;
 `;
-
-const RightPane = styled.div`
-width: 100%;
-max-width: 300px;
-`
 
 const PageDiv = styled.div`
-  max-width: 700px;
+  max-width: 800px;
   margin-left: auto;
   margin-right: auto;
 `;
-const Home = (props) => (
-  <PageDiv>
-    <HeadingStyled>Find a Ride</HeadingStyled>
-    <ContentDiv>
-      <LeftPane>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Source</Form.Label>
-            <Form.Control type="address" placeholder="123 Source Avenue" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Destination</Form.Label>
-            <Form.Control type="address" placeholder="456 Destination Drive" />
-          </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </LeftPane>
-      <RightPane>
-          <img src='https://www.howtogeek.com/wp-content/uploads/2021/01/google-maps-satellite.png' width="300px"/>
-          {/* <iframe src="https://www.google.com/maps/embed/v1/place?key=***&q=Space+Needle,Seattle+WA" width="300px" height="700px"/> */}
-      </RightPane>
-    </ContentDiv>
-  </PageDiv>
-);
+const ButtonDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      src: "",
+      dest: "",
+      routed: -1,
+    };
+    this.map = null;
+    this.renderMap = this.renderMap.bind(this);
+  }
+
+
+  renderMap() {
+    if (this.state.routed == -1) {
+      this.map = (
+        <iframe
+          src={
+            "https://www.google.com/maps/embed/v1/place?key=" +
+            keys.gmaps +
+            "&q=" +
+            "Chicago, IL"
+          }
+          width="500px"
+          height="700px"
+        />
+      );
+    }
+
+    if (this.state.src != "" && this.state.dst != "") {
+      if (this.state.routed == 1) {
+        this.setState({ routed: 2 });
+        this.map = (
+          <iframe
+            src={
+              "https://www.google.com/maps/embed/v1/directions?key=" +
+              keys.gmaps +
+              "&origin=" +
+              this.state.src +
+              "&destination=" +
+              this.state.dest
+            }
+            width="500px"
+            height="700px"
+          />
+        );
+      }
+    }
+  }
+
+  render() {
+    this.renderMap();
+    return (
+      <PageDiv>
+        <ContentDiv>
+          <Pane>
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Source</Form.Label>
+                <Form.Control
+                  type="address"
+                  placeholder="123 Source Avenue"
+                  onChange={(event) =>
+                    this.setState({ src: event.target.value })
+                  }
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Destination</Form.Label>
+                <Form.Control
+                  type="address"
+                  placeholder="456 Destination Drive"
+                  onChange={(event) =>
+                    this.setState({ dest: event.target.value })
+                  }
+                />
+              </Form.Group>
+              <ButtonDiv>
+                <Button
+                  variant="secondary"
+                  type="submit"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    this.setState({ routed: 1 });
+                  }}
+                >
+                  View Route
+                </Button>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    this.setState({ routed: 1 });
+                  }}
+                >
+                  Request Ride
+                </Button>
+              </ButtonDiv>
+            </Form>
+          </Pane>
+          <Pane>{this.map}</Pane>
+        </ContentDiv>
+      </PageDiv>
+    );
+  }
+}
 
 export default Home;
